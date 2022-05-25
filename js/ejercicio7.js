e295ec427d52a674d271b3eb1e8ff813459563cd
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-05-25 17:12:59 -0300

    agrego ejercicio 7

---
+++ js/ejercicio7.js
@@ -0,0 +1,14 @@
+let numero = parseInt(prompt("Ingrese un numero entre 1 y 50: "));
+
+while(numero<0 || numero>50 || isNaN(numero)){
+    numero = parseInt(prompt("Ingrese un numero válido: "));
+}
+
+for(i=numero;i>0;i--){
+    let j=i;
+    while(j>0){
+        document.write(i);
+        j--;
+    }
+    document.write("<br>");
+}
